6002f65779449ab1f7c265e2d11a87829a9bc3e1
Sabas Segovia <sasegovia@ucdavis.edu>
2022-09-28 02:26:55 -0700

    feat: player models complete, todo: status tests on screen

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ window.addEventListener('load', function () {
   const ctx = canvas.getContext('2d');
   canvas.width = 800;
   canvas.height = 720;
-
+  let enemies = [];
+  let score = 0;
   class InputHandler {
     constructor() {
       this.keys = [];
       window.addEventListener('keydown', (e) => {
-        if ((   e.key === "ArrowDown" ||
-                e.key === "ArrowUp" ||
-                e.key === "ArrowLeft" ||
-                e.key === "ArrowRight") &&
-                this.keys.indexOf(e.key) === -1) {
-                this.keys.push(e.key);
+        if ((e.key === "ArrowDown" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowRight") &&
+          this.keys.indexOf(e.key) === -1) {
+          this.keys.push(e.key);
         }
-      })
+      });
       window.addEventListener('keyup', e => {
         if (    e.key === "ArrowDown" ||
                 e.key === "ArrowUp" ||
@@ -33,59 +34,168 @@ window.addEventListener('load', function () {
       this.gameHeight = gameHeight;
       this.width = 200;
       this.height = 200;
-      this.x = 10;
+      this.x = 0;
       this.y = this.gameHeight - this.height;
       this.image = document.getElementById('player');
       this.frameX = 0;
+      this.maxFrame = 8;
       this.frameY = 0;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / this.fps;
       this.speed = 0;
+      this.vy = 0;
+      this.weight = 1;
     }
     draw(context) {
       context.fillStyle = 'white';
-      context.fillRect(this.x, this.y, this.width, this.height);
       context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
     }
-    update(input) {
-      this.x += this.speed;
+    update(input, dt) {
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX >= this.maxFrame) this.frameX = 0;
+        else this.frameX++;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += dt;
+      }
       if (input.keys.indexOf("ArrowRight") > -1) {
         this.speed = 5;
       } else if (input.keys.indexOf("ArrowLeft") > -1) {
         this.speed = -5;
+      } else if (input.keys.indexOf("ArrowUp") > -1 && this.onGround()) {
+        this.vy -= 32
+      } else {
+        this.speed = 0;
       }
 
+      this.x += this.speed;
       if (this.x < 0) this.x = 0;
       else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+
+      this.y += this.vy;
+      if (!this.onGround()) {
+        this.vy += this.weight;
+        this.maxFrame = 5;
+        this.frameY = 1;
+      } else {
+        this.vy = 0;
+        this.maxFrame = 8;
+        this.frameY = 0;
+      }
+      if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+    }
+    onGround() {
+      return this.y >= this.gameHeight - this.height;
     }
 
   }
 
   class Background {
-
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.image = document.getElementById('background');
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.handleEnemies = 720;
+      this.speed = 7;
+    }
+    draw(context) {
+      context.drawImage(this.image, this.x, this.y);
+      context.drawImage(this.image, this.x + this.width - this.speed, this.y);
+    }
+    update() {
+      this.x -= this.speed;
+      if (this.x < 0 - this.width) this.x = 0;
+    }
   }
 
   class Enemy {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 160;
+      this.height = 119;
+      this.image = document.getElementById('enemy');
+      this.x = this.gameWidth;
+      this.y = this.gameHeight - this.height;
+      this.frameX = 0;
+      this.maxFrame = 5;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / this.fps;
+      this.speed = 8;
+      this.markedForDeletion = false;
 
+    }
+    draw(context) {
+      context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+    update(dt) {
+      if (this.frameTimer > this.frameInterval) {
+        if (this.frameX >= this.maxFrame) this.frameX = 0;
+        else this.frameX++;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += dt;
+      }
+      this.x -= this.speed;
+      if (this.x < 0 - this.width) {
+        this.markedForDeletion = true;
+        score++;
+      }
+    }
   }
 
-  function handleEnemies() {
-
+  function handleEnemies(dt) {
+    if (enemyTimer > enemyInterval + randomEnemyInterval) {
+      enemies.push(new Enemy(canvas.width, canvas.height));
+      console.log(enemies);
+      randomEnemyInterval = Math.random() * 1000 + 500;
+      enemyTimer = 0;
+    } else {
+      enemyTimer += dt;
+    }
+    enemies.forEach((enemy) => {
+      enemy.draw(ctx);
+      enemy.update(dt);
+    })
+    enemies = enemies.filter(enemy => !enemy.markedForDeletion);
   }
 
-  function displayStatusText() {
-
+  function displayStatusText(context) {
+    context.font = '40px Helvetica';
+    context.fillStyle = 'black';
+    context.fillText('Score: ' + score, 20, 50);
+    context.fillStyle = 'white';
+    context.fillText('Score: ' + score, 23, 53);
   }
 
   const input = new InputHandler();
   const player = new Player(canvas.width, canvas.height);
-  player.draw(ctx)
+  const background = new Background(canvas.width, canvas.height);
+  const enemy1 = new Enemy(canvas.width, canvas.height);
 
-  function animate() {
+  let lastTime = 0;
+  let enemyTimer = 0;
+  let enemyInterval = 1000;
+  let randomEnemyInterval = Math.random() * 1000 + 500;
+
+  function animate(timeStamp) {
+    const dt = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background.draw(ctx);
+    background.update();
     player.draw(ctx);
-    player.update(input);
+    player.update(input, dt);
+    handleEnemies(dt)
+    displayStatusText(ctx);
     requestAnimationFrame(animate);
   }
 
 
-  animate();
+  animate(0);
 });
