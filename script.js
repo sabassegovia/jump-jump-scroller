5f228f481feed5e3f51c7fae4ad68aa99078940d
Sabas Segovia <sasegovia@ucdavis.edu>
2022-09-28 00:01:36 -0700

    feat: basic left and right movements with boundaries

--- script.js
+++ script.js
@@ -5,10 +5,57 @@ window.addEventListener('load', function () {
   canvas.height = 720;
 
   class InputHandler {
-
+    constructor() {
+      this.keys = [];
+      window.addEventListener('keydown', (e) => {
+        if ((   e.key === "ArrowDown" ||
+                e.key === "ArrowUp" ||
+                e.key === "ArrowLeft" ||
+                e.key === "ArrowRight") &&
+                this.keys.indexOf(e.key) === -1) {
+                this.keys.push(e.key);
+        }
+      })
+      window.addEventListener('keyup', e => {
+        if (    e.key === "ArrowDown" ||
+                e.key === "ArrowUp" ||
+                e.key === "ArrowLeft" ||
+                e.key === "ArrowRight") {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      })
+    }
   }
 
   class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 10;
+      this.y = this.gameHeight - this.height;
+      this.image = document.getElementById('player');
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 0;
+    }
+    draw(context) {
+      context.fillStyle = 'white';
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+    update(input) {
+      this.x += this.speed;
+      if (input.keys.indexOf("ArrowRight") > -1) {
+        this.speed = 5;
+      } else if (input.keys.indexOf("ArrowLeft") > -1) {
+        this.speed = -5;
+      }
+
+      if (this.x < 0) this.x = 0;
+      else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+    }
 
   }
 
@@ -28,8 +75,17 @@ window.addEventListener('load', function () {
 
   }
 
-  function animate() {
+  const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
+  player.draw(ctx)
 
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw(ctx);
+    player.update(input);
+    requestAnimationFrame(animate);
   }
+
+
   animate();
 });
